fb7de1a9fc85be6d47c0691995c0563bb8f9b443
NareszcieCosSieDzieje <48532131+NareszcieCosSieDzieje@users.noreply.github.com>
2020-05-23 15:47:24 +0200

    Add project files.

---
+++ Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Song
+    {
+
+        [Key]
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Artist { set; get; }
+        public int GenreId { set; get; }
+
+       
+    }
+}
